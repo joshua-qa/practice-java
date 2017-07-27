bdba3b0b4614bd5cb8f69031b9a08fa0cca82773
joshua <jgchoi.qa@gmail.com>
2017-07-27 14:46:29 +0900

    Vertex 양방향 등록하도록 변경

--- Synap/Ladder/Game.java
+++ Synap/Ladder/Game.java
@@ -7,7 +7,9 @@ public class Game {
     public void run() {
         ArrayList<Vertex> ladderArray = new ArrayList<>();
         inputVertex(ladderArray);
-        System.out.println(ladderArray.toString());
+        for(Vertex v : ladderArray) {
+            System.out.println("h : " + v.getHeight() + " x: " + v.getCurrentPoint() + " y: " + v.getTargetPoint());
+        }
     }
 
     private void inputVertex(ArrayList<Vertex> ladderArray) {
@@ -25,6 +27,7 @@ public class Game {
                 int x = Integer.parseInt(inputString[1]);
                 int y = x + 1;
                 ladderArray.add(new Vertex(height, x, y));
+                ladderArray.add(new Vertex(height, y, x));
             } catch (Exception e) {
                 System.out.println("다시 입력해주세요(x는 입력 완료).");
                 continue;
