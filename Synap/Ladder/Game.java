e7102aab750d6848e76f4e30cddb1381446ecb55
jgchoiqa <jgchoi.qa@gmail.com>
2017-07-27 16:17:35 +0900

    getDestination 작성, 게임 input 기능 구현, getEdge 및 convertVertex 수정

--- Synap/Ladder/Game.java
+++ Synap/Ladder/Game.java
@@ -1,6 +1,7 @@
 package Joshua.Synap.Ladder;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Game {
@@ -8,29 +9,71 @@ public class Game {
         ArrayList<Vertex> ladderArray = new ArrayList<>();
         inputVertex(ladderArray);
         ArrayList<Vertex>[] ladder = convertVertex(ladderArray, getEdge(ladderArray));
-        for(Vertex v : ladderArray) {
-            System.out.println("h : " + v.getHeight() + " x: " + v.getCurrentPoint() + " y: " + v.getTargetPoint());
+
+        for(int i = 1; i < ladder.length; i++) {
+            System.out.println("array " + i);
+            for (Vertex v : ladder[i]) {
+                System.out.println("h : (" + v.getHeight() + ") x: (" + v.getCurrentPoint() + ") y: (" + v.getTargetPoint() + ")");
+            }
+        }
+
+        while(true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("사다리 정보를 입력하세요(x는 입력 완료).");
+
+            while (true) {
+                String inputString = sc.nextLine();
+                if ("x".equals(inputString)) {
+                    System.out.println("x");
+                    break;
+                }
+
+                try {
+                    int startPoint = Integer.parseInt(inputString);
+                    System.out.println(getDestination(startPoint, 0, ladder));
+                } catch (Exception e) {
+                    System.out.println("다시 입력해주세요(x는 입력 완료).");
+                    continue;
+                }
+            }
         }
     }
 
+    private int getDestination(int startPoint, int height, ArrayList<Vertex>[] ladder) {
+        for(Vertex v : ladder[startPoint]) {
+            int vertexHeight = v.getHeight();
+        }
+        return 0;
+    }
+
     private ArrayList<Vertex>[] convertVertex(ArrayList<Vertex> ladderArray, int sizeOfArray) {
-        ArrayList<Vertex>[] ladder = (ArrayList<Vertex>[]) new ArrayList[sizeOfArray+1];
+        ArrayList<Vertex>[] ladder = (ArrayList<Vertex>[]) new ArrayList[sizeOfArray];
+        for(int i = 1; i < sizeOfArray; i++) {
+            ladder[i] = new ArrayList<>();
+        }
+
+        for(Vertex v : ladderArray) {
+            int index = v.getCurrentPoint();
+            ladder[index].add(v);
+        }
+
+        for(int i = 1; i < sizeOfArray; i++) {
+            ladder[i].sort(Comparator.comparingInt(Vertex::getHeight));
+        }
 
         return ladder;
     }
 
     private int getEdge(ArrayList<Vertex> ladderArray) {
-        int curr = 0;
-        int count = 1;
+        ArrayList<Integer> edgeList = new ArrayList<>();
 
         for(Vertex v : ladderArray) {
-            if(v.getCurrentPoint() != curr) {
-                curr = v.getCurrentPoint();
-                count++;
+            if(!edgeList.contains(v.getCurrentPoint())) {
+                edgeList.add(v.getCurrentPoint());
             }
         }
 
-        return count;
+        return edgeList.size() + 1;
     }
 
     private void inputVertex(ArrayList<Vertex> ladderArray) {
